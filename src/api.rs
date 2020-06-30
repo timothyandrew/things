4639521cb7b850478587d78efb723b0917a1d4b7
Timothy Andrew <mail@timothyandrew.net>
2020-06-30 21:44:01 +0530

    cargo fix + fmt

--- src/api.rs
+++ src/api.rs
@@ -1,138 +1,138 @@
-use serde::{Serialize, Deserialize};
+
 use serde;
-use std::collections::{HashMap, HashSet};
-use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct ChecklistItem {
-  title: String
+    title: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Todo {
-  title: String,
-  notes: Option<String>,
-  when: Option<String>,
-  deadline: Option<String>,
-  tags: Vec<String>,
-  #[serde(rename = "checklist-items")] 
-  checklist_items: Vec<Item>,
-  #[serde(rename = "list-id")] 
-  list_id: Option<String>,
-  list: Option<String>,
-  heading: Option<String>,
+    title: String,
+    notes: Option<String>,
+    when: Option<String>,
+    deadline: Option<String>,
+    tags: Vec<String>,
+    #[serde(rename = "checklist-items")]
+    checklist_items: Vec<Item>,
+    #[serde(rename = "list-id")]
+    list_id: Option<String>,
+    list: Option<String>,
+    heading: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Project {
-  title: String,
-  notes: Option<String>,
-  when: Option<String>,
-  deadline: Option<String>,
-  tags: Vec<String>,
-  #[serde(rename = "area-id")] 
-  area_id: Option<String>,
-  area: Option<String>,
-  items: Vec<Item>
+    title: String,
+    notes: Option<String>,
+    when: Option<String>,
+    deadline: Option<String>,
+    tags: Vec<String>,
+    #[serde(rename = "area-id")]
+    area_id: Option<String>,
+    area: Option<String>,
+    items: Vec<Item>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Heading {
-  title: String,
-  #[serde(default)]
-  archived: bool
+    title: String,
+    #[serde(default)]
+    archived: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type", content = "attributes")]
 pub enum ItemType {
-  #[serde(rename = "to-do")] 
-  Todo(Todo),
+    #[serde(rename = "to-do")]
+    Todo(Todo),
 
-  #[serde(rename = "project")] 
-  Project(Project),
+    #[serde(rename = "project")]
+    Project(Project),
 
-  #[serde(rename = "heading")] 
-  Heading(Heading),
+    #[serde(rename = "heading")]
+    Heading(Heading),
 
-  #[serde(rename = "checklist-item")] 
-  ChecklistItem(ChecklistItem)
+    #[serde(rename = "checklist-item")]
+    ChecklistItem(ChecklistItem),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Operation {
-  #[serde(rename = "update")] 
-  Update,
-  #[serde(rename = "create")] 
-  Create
+    #[serde(rename = "update")]
+    Update,
+    #[serde(rename = "create")]
+    Create,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Item {
-  #[serde(flatten)] 
-  item_type: ItemType,
-  operation: Operation,
-  id: Option<String>,
+    #[serde(flatten)]
+    item_type: ItemType,
+    operation: Operation,
+    id: Option<String>,
 }
 
 // TODO: use the builder pattern instead
 
 pub fn project(title: &str, when: &str, notes: &str, items: Vec<Item>) -> Item {
-  Item { 
-    item_type: ItemType::Project(Project { 
-      title: title.to_string(),
-      notes: Some(notes.to_string()),
-      items: items,
-      when: Some(when.to_string()),
-      ..Project::default()
-    }),
-    id: None,
-    operation: Operation::Create
-  }
+    Item {
+        item_type: ItemType::Project(Project {
+            title: title.to_string(),
+            notes: Some(notes.to_string()),
+            items: items,
+            when: Some(when.to_string()),
+            ..Project::default()
+        }),
+        id: None,
+        operation: Operation::Create,
+    }
 }
 
 pub fn todo(title: &str, note: Option<&str>) -> Item {
-  Item { 
-    item_type: ItemType::Todo(Todo { 
-      title: title.to_string(),
-      notes: note.map(String::from),
-      ..Todo::default()
-    }),
-    id: None,
-    operation: Operation::Create
-  }
+    Item {
+        item_type: ItemType::Todo(Todo {
+            title: title.to_string(),
+            notes: note.map(String::from),
+            ..Todo::default()
+        }),
+        id: None,
+        operation: Operation::Create,
+    }
 }
 
 pub fn todo_checklist(title: &str, note: Option<&str>, checklist: Vec<Item>) -> Item {
-  Item { 
-    item_type: ItemType::Todo(Todo { 
-      title: title.to_string(),
-      notes: note.map(String::from),
-      checklist_items: checklist,
-      ..Todo::default()
-    }),
-    id: None,
-    operation: Operation::Create
-  }
+    Item {
+        item_type: ItemType::Todo(Todo {
+            title: title.to_string(),
+            notes: note.map(String::from),
+            checklist_items: checklist,
+            ..Todo::default()
+        }),
+        id: None,
+        operation: Operation::Create,
+    }
 }
 
 pub fn checklist_item(title: &str) -> Item {
-  Item {
-    item_type: ItemType::ChecklistItem(ChecklistItem {
-      title: title.to_string(),
-    }),
-    id: None,
-    operation: Operation::Create
-  }
+    Item {
+        item_type: ItemType::ChecklistItem(ChecklistItem {
+            title: title.to_string(),
+        }),
+        id: None,
+        operation: Operation::Create,
+    }
 }
 
 pub fn heading(title: &str) -> Item {
-  Item { 
-    item_type: ItemType::Heading(Heading { 
-      title: title.to_string(),
-      ..Heading::default()
-    }),
-    id: None,
-    operation: Operation::Create
-  }
+    Item {
+        item_type: ItemType::Heading(Heading {
+            title: title.to_string(),
+            ..Heading::default()
+        }),
+        id: None,
+        operation: Operation::Create,
+    }
 }
